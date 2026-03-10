bbaeb4ca737838f7ca673d3a42c6b81f970decc7
lzielinski <lukasz.zielinski@nais.pl>
2026-03-10 16:18:29 +0100

    Update method signature to match required library interface

--- Storage/OpenID/AuthorizationCode.php
+++ Storage/OpenID/AuthorizationCode.php
@@ -91,7 +91,7 @@ class AuthorizationCode implements AuthorizationCodeInterface
      * @return void
      * @throws \Exception
      */
-    public function setAuthorizationCode($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null)
+    public function setAuthorizationCode($code, $client_id, $user_id, $redirect_uri, $expires, $scope = null, $id_token = null, $code_challenge = null, $code_challenge_method = null)
     {
         $client = $this->em->getRepository('OAuth2ServerBundle:Client')->find($client_id);
 
